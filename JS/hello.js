a8713986eb07d6d22fe5715a9176aca73426e454
Harsh Thakur <90609542+harshthakur2021@users.noreply.github.com>
2025-07-03 09:57:05 +0530

    Js Basics

---
+++ JS/hello.js
@@ -0,0 +1,14 @@
+console.log("Chai aur code");
+
+function printchai(){
+    console.log("Hello chai")
+}
+
+
+function bringbrush(itne){
+    console.log(`Hello Harsh ${itne}`)
+
+}
+
+printchai()
+bringbrush(4) 
