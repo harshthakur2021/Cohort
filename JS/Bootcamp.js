7b0fe2aa207ed21f0ad1720357891ca97b8bbd0f
Harsh Thakur <90609542+harshthakur2021@users.noreply.github.com>
2025-07-05 11:47:18 +0530

    Data Types

---
+++ JS/Bootcamp.js
@@ -0,0 +1,39 @@
+// String
+
+let name= "Rohit"
+// Age
+let age= 24
+
+// Boolean- true, false
+let ispaid= true
+
+// Null means Empty
+let favoriteClass= null
+// Unddefined - Baad me dekhenge
+let hometown;
+
+//console.log(hometown)
+
+console.log(name)
+
+let skills= ["Html", "CSS", "Java"]
+
+
+let studentProfile ={
+    name: "Harsh",
+    age: 22,
+    ispaid: true,
+    skills:["Html", "CSS", "JS"],
+    favoriteClass: null
+}
+
+// Conditional Statement
+
+let weather= "claudy"
+
+if(weather=== "rainy"){
+console.log("Its Raining")
+}
+else{
+   console.log("Stay in the house")
+}
