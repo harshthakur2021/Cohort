3c9e5aa7b400ec603658d8d85e051ccda23692d3
Harsh Thakur <90609542+harshthakur2021@users.noreply.github.com>
2025-07-12 19:35:15 +0530

    Revision js

---
+++ JS/Shreyn.js
@@ -0,0 +1,15 @@
+let a= [1,2,3,4]
+//console.log(a)
+ a= a.map((e)=> e+1);
+ console.log(a)
+
+let c=  a.filter((e) => e>2)
+console.log(c)
+
+var obj = {
+    name: "harsh",
+    age:12
+}
+console.log(obj.age)
+
+// 
