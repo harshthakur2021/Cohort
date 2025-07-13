b9f1d4a225c4997882fac09ad84483f376776b87
Harsh Thakur <90609542+harshthakur2021@users.noreply.github.com>
2025-07-13 08:31:24 +0530

    Async await done

--- JS/Shreyn.js
+++ JS/Shreyn.js
@@ -12,4 +12,41 @@ var obj = {
 }
 console.log(obj.age)
 
-// 
+// async await ka use krke ap async code ko bhi sync execute kra skta hai
+// aynsc function hmsea ek promise return krega
+
+async function getData(){
+    setTimeout(function(){
+    console.log("Hey I am Harsh Associate Software Engineer")
+    },3000)
+}
+
+ getData();
+
+// await - ?
+
+// fetch API
+
+//Request Types-:
+//1. Get- Jb ap koi data retrive krna chahte hai
+//2. Post- jb ap koi data create krna chahta hai, push krna chahte hai.
+//3. Put- Jb ap koi data existence resources ko update krna chahte hai.
+//4. Delete- Jb ap koi data remove krna chahte hai.
+
+// Scenario:
+// prepare url/ api endpoint -> sync
+// Await// fetch data -> network call -> async
+// process data -> sync
+
+//particular api json return krta hai.. json means javascript object notation...
+ async function getDataa(){
+    // get request - async
+   let response= await fetch('https://jsonplaceholder.typicode.com/https://jsonplaceholder.typicode.com/comments?postId=1');
+   let data =  await response.json(); 
+   console.log(data)
+ } 
+
+ getDataa();
+
+
+ 
